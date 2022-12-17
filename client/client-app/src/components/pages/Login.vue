10f49247a415dc95ec5c30d9e8810e974bce7294
kory-job <catcher.intherye.rk@gmail.com>
2022-12-17 17:21:40 +0900

    login

---
+++ client/client-app/src/components/pages/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p class="text-sky-400 font-bold">ログイン</p>
+    <router-link to="/">トップページへ</router-link>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+  @Component
+  export default class Login extends Vue{}
+</script>
